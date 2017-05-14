66c54c182fcdb51a7578122ddd8b5573fd3fb22c
Thomas Frössman <thomasf@jossystem.se>
2017-05-14 13:46:12 +0200

    Send kwargs to configure_user function

--- djangooidc/backends.py
+++ djangooidc/backends.py
@@ -52,7 +52,7 @@ class OpenIdConnectBackend(ModelBackend):
                     'defaults': openid_data, }
             user, created = UserModel.objects.update_or_create(**args)
             if created:
-                user = self.configure_user(user)
+                user = self.configure_user(user, **kwargs)
         else:
             try:
                 user = UserModel.objects.get_by_natural_key(username)
